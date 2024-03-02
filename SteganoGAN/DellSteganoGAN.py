104e7300cd1dd4aa30c858336abc0b4514058628
HackTrick2024 <hacktrick2024@gmail.com>
2024-03-02 16:49:40 +0200

    Modify image extensions, add missing import in SteganoGAN

--- SteganoGAN/DellSteganoGAN.py
+++ SteganoGAN/DellSteganoGAN.py
@@ -1,7 +1,5 @@
 from utils import *
-from torchvision.utils import save_image
-import os
-import time
+import numpy as np
 
 def stegano_solver(cover_im: np.ndarray, message: str) -> str:
     ## add your code here
